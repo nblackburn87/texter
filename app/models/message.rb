d6b52d28c5a5f4a3ac9286350f772044115ce236
Paul Ogawa & Natalie Blackburn <student@epicodus.com>
2014-04-21 10:03:41 -0700

    add needed gems, create basic app views

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,9 +6,9 @@ private
   def send_message
     response = RestClient::Request.new(
       :method => :post,
-      :url => '',
-      :user => '',
-      :password => '',
+      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => { :Body => body,
                     :To => to,
                     :From => from }
